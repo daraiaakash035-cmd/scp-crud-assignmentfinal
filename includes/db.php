8083d41c0aebab43be80198d6071fd2c027f5bab
daraiaakash035-cmd <daraiaakash035@gmail.com>
2026-06-09 14:16:13 +1200

    db.php

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,12 @@
 <?php
-
 $host = "localhost";
-$user = "a30105201_a30105201";
-$password = "Toiohomai1234";
-$database = "a30105201_scp_database";
+$user = "root";
+$password = "";
+$database = "scp_database";
 
 $conn = mysqli_connect($host, $user, $password, $database);
 
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    die("Database connection failed: " . mysqli_connect_error());
 }
-
 ?>
